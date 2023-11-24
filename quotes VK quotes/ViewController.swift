337c138984bff372b56f01c9b28c3f71b1548535
Nikita <nik007rev@gmail.com>
2023-11-24 14:35:00 +0300

    Initial Commit

---
+++ quotes VK quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quotes VK quotes
+//
+//  Created by Никита  on 24.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
